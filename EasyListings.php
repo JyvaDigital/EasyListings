713b5b5409a718e398bb643f186e5bb36e9ce081
VilleJyva <72099025+VilleJyva@users.noreply.github.com>
2020-12-28 13:39:41 +0200

    Admin menu test

--- EasyListings.php
+++ EasyListings.php
@@ -36,6 +36,9 @@ class EasyListingsPlugin{
 
     function deactivate(){
 
+        // Clear the permalinks to remove our post type's rules from the database.
+        flush_rewrite_rules();
+
     }
 
     function uninstall(){
@@ -43,8 +46,41 @@ class EasyListingsPlugin{
     }
 
 }
+
+function wporg_options_page_html() {
+    ?>
+    <div class="wrap">
+      <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+      <form action="options.php" method="post">
+        <?php
+        // output security fields for the registered setting "wporg_options"
+        settings_fields( 'wporg_options' );
+        // output setting sections and their fields
+        // (sections are registered for "wporg", each field is registered to a specific section)
+        do_settings_sections( 'wporg' );
+        // output save settings button
+        submit_button( __( 'Save Settings', 'textdomain' ) );
+        ?>
+      </form>
+    </div>
+    <?php
+}
+
+add_action( 'admin_menu', 'wporg_options_page' );
+function wporg_options_page() {
+    add_menu_page(
+        'WPOrg',
+        'WPOrg Options',
+        'manage_options',
+        'wporg',
+        'wporg_options_page_html',
+        plugin_dir_url(__FILE__) . 'images/icon_wporg.png',
+        20
+    );
+}
+
 if(class_exists('EasyListingsPlugin')){
-    $EasylistingsPlugin = new EasyListingsPlugin('Easy Listing initialized');
+    $EasyListingsPlugin = new EasyListingsPlugin('Easy Listing initialized');
 }
 
 register_activation_hook(__FILE__,array($EasyListingsPlugin, 'activate') );
